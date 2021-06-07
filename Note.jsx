cf835dc2a84846fe8c6d802d142a554dc0018af1
BABU-BALAJI-V <85435869+BABU-BALAJI-V@users.noreply.github.com>
2021-06-07 09:19:48 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This is was an amazing bootcamo by Saurya Sinha. Thank you for teaching 
+        with clarity and patience from scratch.
+      </p>
+    </div> 
+  );
+}
+export default Note;
